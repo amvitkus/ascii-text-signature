92af7a498f97f2b5dd31b231393757f947e4f963
Aras <amvitkus@gmail.com>
2020-12-12 13:14:42 -0500

    Progress on Stage 2, Star quality.

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,8 +1,15 @@
 package signature
 
+import java.util.*
+
 fun main() {
-    println("_____________")
-    println("| Hyper Skill |")
-    println(" •••••••••••••")
+    val scanner = Scanner(System.`in`)
+    val name = scanner.nextLine()
+    val border = "*"
+    val borderLength = name.length
+
+    println("**" + name.length + "**")
+//    println("| Hyper Skill |")
+//    println(" •••••••••••••")
 
 }
