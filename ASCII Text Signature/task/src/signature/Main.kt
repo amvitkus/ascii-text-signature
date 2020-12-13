c37a17ae1875948c163cc3c3464c416d57350f91
Aras <amvitkus@gmail.com>
2020-12-13 02:39:09 -0500

    Completed Stage 2.

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -20,6 +20,26 @@ fun main() {
 //        print("**" + border + "**")
 
     }
-    println("* $name *")
+    print("\n")
+    print("* $name *")
+    print("\n")
+    print("****")
+
+    for (i in 0 until borderLength) {
+        print(border)
+//        println("* $name *")
+//        print("**" + border + "**")
+
+    }
+
+    //Code Below is a better way of solving it.
+//    val ch = scanner.nextLine()
+//
+//    val lg: Int = ch.length
+//    val str: String = "*".repeat(lg)
+//
+//    println("**$str**")
+//    println("* $ch *")
+//    println("**$str**")
 
 }
