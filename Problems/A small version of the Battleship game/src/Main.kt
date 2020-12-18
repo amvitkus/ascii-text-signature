c4531cd70d1e18dd4dec4af5fb5154b3dcd9ef3e
Aras <amvitkus@gmail.com>
2020-12-18 12:33:44 -0500

    Start of small battleship game.

--- Problems/A small version of the Battleship game/src/Main.kt
+++ Problems/A small version of the Battleship game/src/Main.kt
@@ -3,4 +3,25 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
     // put your code here
+    //val myArray = input.IntArray(6)
+    val x1 = input.nextInt()
+    val y1 = input.nextInt()
+    val x2 = input.nextInt()
+    val y2 = input.nextInt()
+    val x3 = input.nextInt()
+    val y3 = input.nextInt()
+
+    var rows = ""
+    var columns = ""
+
+    for (x in 1..5) {
+        if ()
+    }
+
+    for (y in 1..5) {
+
+    }
+
+    println(rows.trim())
+    println(columns.trim())
 }
