b05c83a0fa32c54f22736f43bfc380d2c81f15fb
Aras <amvitkus@gmail.com>
2020-12-21 21:33:34 -0500

    Cube of zeroes, 3d array

---
+++ Problems/Cube of zeros/src/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val threeDArray = arrayOf(
+        arrayOf(
+            arrayOf(0, 0, 0),
+            arrayOf(0, 0, 0),
+            arrayOf(0, 0, 0)
+        ),
+        arrayOf(
+            arrayOf(0, 0, 0),
+            arrayOf(0, 0, 0),
+            arrayOf(0, 0, 0)
+        ),
+        arrayOf(
+            arrayOf(0, 0, 0),
+            arrayOf(0, 0, 0),
+            arrayOf(0, 0, 0)
+        )
+    )
+
+    println(threeDArray.contentDeepToString())
+
+}
