d67264949b4e92efe2878fbb39d35f3ba343c4ac
Aras <amvitkus@gmail.com>
2020-12-24 12:16:09 -0500

    Some progress on balance checker.

--- Problems/Balance checker/src/Main.kt
+++ Problems/Balance checker/src/Main.kt
@@ -2,5 +2,25 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
-    
+
+    // write your code here
+    var initial = scanner.nextInt()
+
+    //while (scanner.hasNextInt()) {
+        var a = scanner.nextInt()
+        var b = scanner.nextInt()
+        var c = scanner.nextInt()
+        var purchase = a + b + c
+        val remainder = initial - purchase
+        val required = remainder + purchase - remainder
+    //}
+
+    print("Thank you for choosing us to manage your account! You have $remainder money.\n")
+    initial = remainder
+
+    if (initial <= 0) {
+        println("Error, insufficient funds for the purchase. You have $initial, but you need $required.")
+    }
+
 }
+
