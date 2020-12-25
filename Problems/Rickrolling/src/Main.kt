d03dbd2a10b00fdcf7f1e106c0f9c4efd04dfc57
arasvitkus <amvitkus@gmail.com>
2020-12-25 13:59:35 -0500

    Problem of the day, rickrolling solved.

---
+++ Problems/Rickrolling/src/Main.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() {
+    // put your code here
+    val scanner = Scanner(System.`in`)
+    val word1 = scanner.next()
+    val word2 = scanner.next()
+    val word3 = scanner.next()
+    val word4 = scanner.next()
+    val word5 = scanner.next()
+
+    println("$word1 $word2 $word3 $word4 $word5")
+}
