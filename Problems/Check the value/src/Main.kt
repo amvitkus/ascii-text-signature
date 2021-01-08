ab648d8af79dee489e09029ca087dbff5aca40b0
Aras <amvitkus@gmail.com>
2021-01-07 20:55:26 -0500

    Problem of the day, using if to check if a number is less than 10.

---
+++ Problems/Check the value/src/Main.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    // put your code here
+    val scanner = Scanner(System.`in`)
+
+    val num = scanner.nextInt()
+
+    if (num < 10) {
+        println("true")
+    } else {
+        println("false")
+    }
+}
--- Problems/Queens/src/Main.kt
+++ Problems/Queens/src/Main.kt
@@ -1,7 +1,3 @@
-import java.util.*
-
 fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)
-
     // write your code here
 }
