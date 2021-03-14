fe6d8c41e1d524caec8699624261b1662907d9fb
Aras <amvitkus@gmail.com>
2021-03-14 15:50:48 -0400

    Commit for unversioned files update.

--- Problems/Shapeshifter/src/Main.kt
+++ Problems/Shapeshifter/src/Main.kt
@@ -1,3 +1,11 @@
-// Initialization of inputArray. Do not declare this array!
+fun main() {
+    //Do not touch code below
+    var inputArray: Array<Array<String>> = arrayOf()
+    val n = readLine()!!.toInt()
+    for (i in 0 until n) {
+        val strings = readLine()!!.split(' ').toTypedArray()
+        inputArray += strings
+    }
+    // write your code here    
 
-TODO()
+}
